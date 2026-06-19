90f9170921d88962ece3c7bcfbbbce3583efd1cb
Almas Banu <almasbanuu234@gmail.com>
2026-06-19 22:35:09 +0530

    Enhance password strength checker to v2

--- password-strength-checker.js
+++ password-strength-checker.js
@@ -1,5 +1,5 @@
 // Sample password data
-let password = "Secure123";
+let password = "SecurePassword123";
 let hasUppercase = true;
 let hasNumber = true;
 
@@ -9,18 +9,24 @@ function checkPasswordStrength(
     hasUppercase,
     hasNumber
 ) {
-    if (password.length >= 8 && hasUppercase && hasNumber) {
+    // Strong condition
+    if(password.length >= 12 && hasUppercase && hasNumber){
         return "Strong Password";
     }
+    // Medium condition
+    else if(password.length >= 8){
+        return "Medium Strength Password";
+    }
+    // Weak condition
+    else{
+        return "Weak Strength Password";
+    }
 
-    return "Weak Password";
 }
 
 // Display result
-console.log(
-    checkPasswordStrength(
-        password,
-        hasUppercase,
-        hasNumber
-    )
-);
+console.log(checkPasswordStrength(
+    password,
+    hasUppercase,
+    hasNumber
+));
