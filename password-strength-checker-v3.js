3a57272d0d07505a4faf81cb405e8fc38a090adb
Almas Banu <almasbanuu234@gmail.com>
2026-07-10 06:29:39 +0530

    Refine password strength checker v3 comments and formatting

--- password-strength-checker-v3.js
+++ password-strength-checker-v3.js
@@ -5,13 +5,6 @@
 function checkPasswordStrength(password) {
     // Flags to track password requirements
     let hasUppercase = false;
-    let hasNumber = false;// Password Strength Checker - Version 3
-// This version automatically detects uppercase letters and numbers
-// using loops instead of relying on manually provided boolean values.
-
-function checkPasswordStrength(password) {
-    // Flags to track password requirements
-    let hasUppercase = false;
     let hasNumber = false;
 
     // Check each character in the password
@@ -28,7 +21,7 @@ function checkPasswordStrength(password) {
         }
     }
 
-    // Determine password strength
+    // Return the password strength based on the collected flags
     if (password.length >= 8 && hasUppercase && hasNumber) {
         return "Strong Password";
     }
